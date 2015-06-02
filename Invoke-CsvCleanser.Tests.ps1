f4238a281bb98815b3a33c5484a851d62b25e8f7
Craig Buchanan <craig.buchanan@choa.org>
2015-06-02 17:01:48 -0400

    initial commit

---
+++ Invoke-CsvCleanser.Tests.ps1
@@ -0,0 +1,35 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Invoke-CsvCleanser" {
+
+    $nl = [Environment]::NewLine
+
+    # create CSV file; populate it with data
+    $csv = "TestDrive:\0000.csv"
+    Set-Content $csv -Value 'DATE_COLUMN,DATETIME_COLUMN,TEXT_COLUMN'
+    Add-Content $csv -Value '2015-05-01,2015-05-01 23:00:00.000,LOREM IPSUM'
+    Add-Content $csv -Value 'NULL,null,nuLL'
+       
+    It "Should remove all instances of the word 'NULL'" {
+        Invoke-CsvCleanser -path $csv -nulls
+
+        $actual = Get-Content $csv
+        Write-Host $actual
+        $expected = '"DATE_COLUMN","DATETIME_COLUMN","TEXT_COLUMN"' + $nl + '"2015-05-01","2015-05-01 23:00:00.000","LOREM IPSUM"' + $nl + '"","","",""'
+
+        $actual | Should Be $expected
+    }
+
+    It "Should remove milliseconds from all datetimes" {
+        Invoke-CsvCleanser -path $csv -Milliseconds
+
+        $actual = Get-Content $csv
+        Write-Host $actual
+        $expected = '"DATE_COLUMN","DATETIME_COLUMN","TEXT_COLUMN"' + $nl + '"2015-05-01","2015-05-01 23:00:00","LOREM IPSUM"' + $nl + '"NULL","null","nuLL"'
+
+        $actual | Should Be $expected
+    }
+
+}
---
+++ Invoke-CsvCleanser.ps1
@@ -0,0 +1,67 @@
+﻿function IsDate($object) {
+    [Boolean]($object -as [DateTime])
+}
+
+function Invoke-CsvCleanser {
+
+    
+  [CmdletBinding()]
+  Param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $Path,
+    [switch]
+    $Nulls,
+    [switch]
+    $Milliseconds
+  )
+
+  BEGIN {
+    Write-Verbose "$($MyInvocation.MyCommand.Name)::Begin" 
+
+#    Write-Verbose $Path
+#    Write-Verbose $Nulls
+#    Write-Verbose $Milliseconds
+
+  }
+
+  PROCESS {
+    Write-Verbose "$($MyInvocation.MyCommand.Name)::Process"
+
+    # open the file
+    $data = Import-Csv $path
+
+    # process each row
+    $data | Foreach-Object { 
+
+        # process each column
+        Foreach ($property in $_.PSObject.Properties) {
+
+            Write-Verbose ("Raw: {0}: {1}" -f $property.Name, $property.Value)
+
+            # if column contains 'NULL', replace it with ''
+            if ($Nulls -and ($property.Value -eq 'NULL')) {
+
+                $property.Value = $property.Value -replace 'NULL', ''
+                Write-Debug ("-Nulls: {0}: {1}" -f $property.Name, $property.Value)
+
+            }
+            
+            # if column contains a date/time value, remove milliseconds
+            elseif ( $Milliseconds -and (isDate($property.Value)) ) {
+
+                $property.Value = $property.Value -replace '.000', ''
+
+                Write-Debug ("-Milliseconds: {0}: {1}" -f $property.Name, $property.Value)
+            }
+        } 
+
+    } 
+    
+    # save file
+    $data | Export-Csv -Path $Path -NoTypeInformation
+    
+  }
+  END { Write-Verbose "$($MyInvocation.MyCommand.Name)::End"}
+
+}
