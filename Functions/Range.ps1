2d0180d9a579a843501c1fd7bd84dc4f74260ce9
Craig Buchanan <craig.buchanan@choa.org>
2015-10-21 17:53:32 -0400

    correcting Range implmentation

--- Functions/Range.ps1
+++ Functions/Range.ps1
@@ -1,72 +1,20 @@
-<#
-.SYNOPSIS
-Calculate the minimum and maximum value of the specified fields.
+Filter Range {
 
-.PARAMETER Path
-The CSV file to be processed.
-
-.PARAMETER Fields
-Array of fields to be processed.
-
-.EXAMPLE
-PS> Range -p path\to\file.csv -f 'FIELD_0','FIELD_1'
-
-.RETURNS
-PSCustomObject
-
-#>
-Function Range {
-
-    [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$True,Position=1)]
-        [alias('p')]
-        [String] $Path,
-
-        [Parameter(Mandatory=$True,Position=2)]
-        [alias('f')]
-        [String[]] $Fields
+        [String] $File
     )
 
-    BEGIN {
-$QueryTemplate=@"
-    SELECT  Min({0}) AS MINIMUM, Max({0}) AS MAXIMUM
-    FROM    {1}
-"@
+    $Query = "SELECT Min([$($_.Name)]) AS Minimum, Max([$($_.Name)]) AS Maximum FROM '$File'"
 
-#    WHERE   {0} <> 'NULL'
+    [xml] $xml = & logparser $Query -stats:off -o:xml
 
-        Write-Debug $querytemplate
+    $Measurement=[PsCustomObject]@{'Name'='Minimum';Value=$xml.root.row.Minimum.Trim()}
+    $_.Scalars+=$Measurement
 
-    }
-    PROCESS {
+    $Measurement=[PsCustomObject]@{'Name'='Maximum';Value=$xml.root.row.Maximum.Trim()}
+    $_.Scalars+=$Measurement
 
-        Foreach ($Field in $Fields) {
-          Write-Verbose $Field
-
-            $Query = ($QueryTemplate -f $Field, $Path)
-            Write-Debug $Query
-
-            # create standard container
-            $Container = [PSCustomObject]@{Field=$Field;Computation='Range';Values=$null}
-
-            [xml] $xml = & logparser $Query -stats:off -o:xml
-
-            # process results; assign to container's Value property
-            $Container.values = $xml.root.row | Foreach {
-                Write-Verbose "Minimum: $($_.Minimum.Trim()); Maximum: $($_.Maximum.Trim())"
-                [PSCustomObject]@{
-                    Minimum=$_.Minimum.Trim(); 
-                    Maximum=$_.Maximum.Trim()
-                }
-            }
-
-            # return container to pipeline
-            $Container
-
-        } #Foreach
-
-    } # PROCESS
-    END {}
+    # return object to pipeline
+    # $_
 
 }
